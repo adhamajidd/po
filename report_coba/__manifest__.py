9edcabf573f3e4ec270a1d5d27aac8df274c715e
adhamajidd <majid.abajoo@gmail.com>
2025-08-07 16:30:48 +0700

    coba

---
+++ report_coba/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Report PDF Majid',
+    'version': '1.0',
+    'description': 'company report pdf',
+    'author': 'Majid Abajoo',
+    'website': 'https://www.adhacompany.com',
+    'license': 'LGPL-3',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'report/report_property_usear.xml',
+        'report/report.xml',
+
+    ],
+    'auto_install': False,
+    'application': False,
+    'assets': {
+        
+    }
+}
