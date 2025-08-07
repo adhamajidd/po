7da7a825c396e6608d986400a7868f56577e0229
adhamajidd <majid.abajoo@gmail.com>
2025-08-07 15:46:07 +0700

    Add files via upload

---
+++ abj_report/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Abajoo Custom Invoice - New Report",
+    "version": "1.0",
+    "depends": ["account"],
+    "category": "Accounting",
+    "summary": "New standalone PDF invoice report using QWeb",
+    "data": [
+        "report/new_invoice_templates.xml",
+        "views/report_action.xml",
+    ],
+    "installable": True,
+}
