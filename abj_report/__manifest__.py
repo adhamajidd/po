2740022f620805e0b3727c83262cbe79a033487a
adhamajidd <majid.abajoo@gmail.com>
2025-08-07 15:51:44 +0700

    Add files via upload

--- abj_report/__manifest__.py
+++ abj_report/__manifest__.py
@@ -5,8 +5,9 @@
     "category": "Accounting",
     "summary": "New standalone PDF invoice report using QWeb",
     "data": [
-        "report/new_invoice_templates.xml",
+        "reports/new_invoice_templates.xml",
         "views/report_action.xml",
     ],
     "installable": True,
+    "icon": "abj_report/static/src/img/icon.png",
 }
